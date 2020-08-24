c2e12a307d96cfe9a3bed348d824a6faa1fabc2d
TIANYUZHOU1030 <70006218+TIANYUZHOU1030@users.noreply.github.com>
2020-08-24 16:47:53 +0800

    upload xyz

---
+++ xyz/assets/js/main.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+
+var word1 = [
+	"Groot",
+	"Harry Potter",
+	"Minions"
+];
+
+var randomWord1 = word1[Math.floor(Math.random()*word1.length)];
+
+var word2 = [
+  "sing “7 rings”",
+  "follow instagram",
+  "speak tongue twister "
+];
+
+var randomWord2 = word2[Math.floor(Math.random()*word2.length)];
+
+var word3 = [
+  "Flash",
+  "Iron Man",
+  "E.T."
+];
+
+var randomWord3 = word3[Math.floor(Math.random()*word3.length)];
+
+var word4 = [
+	"Gollum",
+	"Wolverine",
+	"Batman"
+];
+
+var randomWord4 = word4[Math.floor(Math.random()*word4.length)];
+
+var word5 = [
+  "go to clubs",
+  "pick a sweet apple",
+  "role exchange"
+];
+
+var randomWord5 = word5[Math.floor(Math.random()*word5.length)];
+
+var word6 = [
+  "Snow White",
+  "Captain America",
+  "Miranda Priestly"
+];
+
+var randomWord6 = word6[Math.floor(Math.random()*word6.length)];
+
+var word7 = [
+	"Cinderella",
+	"Loki",
+	"Captain Jack Sparrow"
+];
+
+var randomWord7 = word7[Math.floor(Math.random()*word7.length)];
+
+var word8 = [
+  "save the world",
+  "play poker",
+  "watch a horror movie "
+];
+
+var randomWord8 = word8[Math.floor(Math.random()*word8.length)];
+
+var word9 = [
+  "The Joker",
+  "Spiderman",
+  "The Mask"
+];
+
+var randomWord9 = word9[Math.floor(Math.random()*word9.length)];
+
+var word10 = [
+	"Doraemon",
+	"Kevin McCallister",
+	"Nick and Judy "
+];
+
+var randomWord10 = word10[Math.floor(Math.random()*word10.length)];
+
+var word11 = [
+  "go to the sea world",
+  "swimming compete",
+  "bungee jumping "
+];
+
+var randomWord11 = word11[Math.floor(Math.random()*word11.length)];
+
+var word12 = [
+  "Elsa",
+  "Nemo ",
+  "Avatar"
+];
+
+var randomWord12 = word12[Math.floor(Math.random()*word12.length)];
+
+var word13 = [
+	"Homer Simpson",
+	"Mickey Mouse",
+	"Tom and Jerry "
+];
+
+var randomWord13 = word13[Math.floor(Math.random()*word13.length)];
+
+var word14 = [
+  "share the cheese",
+  "perform a musical",
+  "plan a road trip "
+];
+
+var randomWord14 = word14[Math.floor(Math.random()*word14.length)];
+
+var word15 = [
+  "Mr. Bean",
+  "The Phantom ",
+  "James Bond"
+];
+
+var randomWord15 = word15[Math.floor(Math.random()*word15.length)];
+
+$( ".word1" ).html(randomWord1);
+$( ".word2" ).html(randomWord2);
+$( ".word3" ).html(randomWord3);
+$( ".word4" ).html(randomWord4);
+$( ".word5" ).html(randomWord5);
+$( ".word6" ).html(randomWord6);
+$( ".word7" ).html(randomWord7);
+$( ".word8" ).html(randomWord8);
+$( ".word9" ).html(randomWord9);
+$( ".word10" ).html(randomWord10);
+$( ".word11" ).html(randomWord11);
+$( ".word12" ).html(randomWord12);
+$( ".word13" ).html(randomWord13);
+$( ".word14" ).html(randomWord14);
+$( ".word15" ).html(randomWord15);
+
+
+});
